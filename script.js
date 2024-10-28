149b8a668cb94b02d0a120b39fdf9d388a347d21
Biswojit Biswal <120778980+biswojitbiswal@users.noreply.github.com>
2024-10-28 09:51:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+let draggedElement = null;
+
+document.querySelectorAll('.draggable').forEach((element) => {
+  element.addEventListener('dragstart', (e) => {
+    draggedElement = e.target;
+  });
+
+  element.addEventListener('dragover', (e) => {
+    e.preventDefault();
+  });
+
+  element.addEventListener('drop', (e) => {
+    e.preventDefault();
+    if (draggedElement !== e.target) {
+      const tempBackground = e.target.style.backgroundImage;
+      e.target.style.backgroundImage = draggedElement.style.backgroundImage;
+      draggedElement.style.backgroundImage = tempBackground;
+    }
+  });
+});
